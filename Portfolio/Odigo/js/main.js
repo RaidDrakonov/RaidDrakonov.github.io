6a4b1d8f3f4d03ba177ffffea8cadf78d4986425
Maxim Kozhanovskii <38167107+RaidDrakonov@users.noreply.github.com>
2021-03-22 23:12:12 +0200

    TZ layout

--- Portfolio/Odigo/js/main.js
+++ Portfolio/Odigo/js/main.js
@@ -3,6 +3,26 @@ document.querySelector('.menu-icon-wrapper').onclick = function(){
   document.querySelector('.header__nav').classList.toggle('header__nav--visible');
 }
 
+
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.header__nav');
+  const menuCloseItem = document.querySelector('.header__nav-close');
+  const menuLinks = document.querySelectorAll('.header__link');
+  burgerItem.addEventListener('click', () => {
+      menu.classList.add('header__nav_active');
+  });
+  menuCloseItem.addEventListener('click', () => {
+      menu.classList.remove('header__nav_active');
+  });
+  if (window.innerWidth <= 767) {
+      for (let i = 0; i < menuLinks.length; i += 1) {
+          menuLinks[i].addEventListener('click', () => {
+              menu.classList.remove('header__nav_active');
+          });
+      }
+  }
+}());
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
     smoothLink.addEventListener('click', function (e) {
---
+++ Portfolio/test-layout/js/main.js
@@ -0,0 +1,8 @@
+$('.slider__items').slick({
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: true, 
+  dots: true
+});
+
+document.getElementById("copy").innerHTML = "Сopyrigth 2021";
